241c0e60ba80d1ee479879abb5e68215afaa4b54
Arnah <github@arnah.ca>
2024-02-07 17:48:32 -0700

    Bump version to 1.1 for current changes

--- HaCreator/GUI/About.cs
+++ HaCreator/GUI/About.cs
@@ -21,7 +21,7 @@ namespace HaCreator.GUI {
 		}
 
 		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
-			Process.Start("https://github.com/lastbattle/Harepacker-resurrected");
+			Process.Start("https://github.com/Arnuh/HaSuite/");
 		}
 	}
 }
