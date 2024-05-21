Arnuh/HaSuite

commit e0a9a2351935260f99a8c1dd4f55280a97948ed4

[MapleLib] Convert list encrypted entries when iv changes

MapleLib/WzLib/WzDirectory.cs
@@ -325,14 +325,11 @@ namespace MapleLib.WzLib {
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <param name="listWzPath">The path of the List.wz used for encrypted paths</param>
-		/// <param name="useIv">The IV to use while generating the data file. If null, it'll use the WzDirectory default</param>
-		/// <param name="bIsWzUserKeyDefault">Uses the default MapleStory UserKey or a custom key.</param>
+		/// <param name="isWzIvSimilar">If the WzIv is changed then images need to be updated</param>
+		/// <param name="isWzUserKeyDefault">Uses the default MapleStory UserKey or a custom key.</param>
 		/// <param name="prevOpenedStream">The previously opened file stream</param>
 		/// <returns></returns>
-		internal int GenerateDataFile(byte[] useIv, bool bIsWzUserKeyDefault, FileStream prevOpenedStream) {
-			var useCustomIv = useIv != null; // whole shit gonna be re-written if its a custom IV specified
-
+		internal int GenerateDataFile(bool isWzIvSimilar, bool isWzUserKeyDefault, FileStream prevOpenedStream) {
 			size = 0;
 			var entryCount = subDirs.Count + images.Count;
 			if (entryCount == 0) {
@@ -344,13 +341,13 @@ namespace MapleLib.WzLib {
 			offsetSize = WzTool.GetCompressedIntLength(entryCount);
 
 			foreach (var img in images) {
-				if (useCustomIv || // everything needs to be re-written when a custom IV is used.
-				    !bIsWzUserKeyDefault || //  everything needs to be re-written when a custom UserKey is used too
+				if (!isWzIvSimilar || // everything needs to be re-written when a custom IV is used.
+				    !isWzUserKeyDefault || //  everything needs to be re-written when a custom UserKey is used too
 				    img.bIsImageChanged) // or when an image is changed
 				{
 					using (var memStream = new MemoryStream()) {
-						using (var imgWriter = new WzBinaryWriter(memStream, useCustomIv ? useIv : WzIv, UserKey)) {
-							img.SaveImage(imgWriter, bIsWzUserKeyDefault, useCustomIv);
+						using (var imgWriter = new WzBinaryWriter(memStream, WzIv, UserKey)) {
+							img.SaveImage(imgWriter, isWzIvSimilar, isWzUserKeyDefault, !isWzIvSimilar);
 
 							img.CalculateAndSetImageChecksum(memStream.ToArray()); // checksum
 
@@ -379,9 +376,7 @@ namespace MapleLib.WzLib {
 				offsetSize += 4;
 
 				// otherwise Item.wz (300MB) probably uses > 4GB
-				if (useCustomIv ||
-				    !bIsWzUserKeyDefault) // when using custom IV, or changing IVs, all images have to be re-read and re-written..
-				{
+				if (!isWzIvSimilar || !isWzUserKeyDefault) {// when using custom IV, or changing IVs, all images have to be re-read and re-written..
 					GC.Collect(); // GC slows down writing of maps in HaCreator
 					GC.WaitForPendingFinalizers();
 				}
@@ -392,7 +387,7 @@ namespace MapleLib.WzLib {
 			foreach (var dir in subDirs) {
 				var nameLen = WzTool.GetWzObjectValueLength(dir.name, 3);
 				size += nameLen;
-				size += dir.GenerateDataFile(useIv, bIsWzUserKeyDefault, prevOpenedStream);
+				size += dir.GenerateDataFile(isWzIvSimilar, isWzUserKeyDefault, prevOpenedStream);
 				size += WzTool.GetCompressedIntLength(dir.size);
 				size += WzTool.GetCompressedIntLength(dir.Checksum);
 				size += 4;

MapleLib/WzLib/WzFile.cs
@@ -589,7 +589,7 @@ namespace MapleLib.WzLib {
 				var tempFile = Path.GetFileNameWithoutExtension(path) + ".TEMP";
 
 				using (var fs = new FileStream(tempFile, FileMode.Append, FileAccess.Write)) {
-					wzDir.GenerateDataFile(isWzIvSimilar ? null : WzIv, isWzUserKeyDefault, fs);
+					wzDir.GenerateDataFile(isWzIvSimilar, isWzUserKeyDefault, fs);
 				}
 
 				WzTool.StringCache.Clear();

MapleLib/WzLib/ListWzContainer.cs
@@ -74,5 +74,27 @@ namespace MapleLib.WzLib {
 				MarkListWzProperty(prop, listWz);
 			}
 		}
+		
+		public static void ConvertKey(WzImage image) {
+			foreach (var prop in image.WzProperties) {
+				ConvertKey(prop);
+			}
+		}
+		
+		public static void ConvertKey(WzImageProperty parent, WzMutableKey decWzKey = null, WzMutableKey encWzKey = null) {
+			if (parent.WzProperties == null) return;
+			foreach (var prop in parent.WzProperties) {
+				if (prop is WzCanvasProperty canvas) {
+					var pngProp = canvas.PngProperty;
+					var key = parent.ParentImage?.wzKey ?? decWzKey;
+					var listWz = canvas.PngProperty.CheckListWzUsed();
+					if (listWz) {
+						pngProp.ConvertCompressed(key, listWz ? encWzKey : null, false);
+					}
+				}
+
+				ConvertKey(prop, decWzKey, encWzKey);
+			}
+		}
 	}
 }

MapleLib/WzLib/WzImage.cs
@@ -453,13 +453,12 @@ namespace MapleLib.WzLib {
 		/// Writes the WzImage object to the underlying WzBinaryWriter
 		/// </summary>
 		/// <param name="writer"></param>
-		/// <param name="bIsWzUserKeyDefault">Uses the default MapleStory UserKey or a custom key.</param>
+		/// <param name="isWzUserKeyDefault">Uses the default MapleStory UserKey or a custom key.</param>
 		/// <param name="forceReadFromData">Read from data regardless of base data that's changed or not.</param>
-		public void SaveImage(WzBinaryWriter writer, bool bIsWzUserKeyDefault = true, bool forceReadFromData = false) {
+		public void SaveImage(WzBinaryWriter writer, bool isWzIvSimilar = true, bool isWzUserKeyDefault = true, bool forceReadFromData = false) {
 			if (bIsImageChanged ||
-			    !bIsWzUserKeyDefault || //  everything needs to be re-written when a custom UserKey is used
-			    forceReadFromData) // if its not being force-read and written, it saves with the previous WZ encryption IV.
-			{
+			    !isWzUserKeyDefault || //  everything needs to be re-written when a custom UserKey is used
+			    forceReadFromData) { // if its not being force-read and written, it saves with the previous WZ encryption IV.
 				if (reader != null && !parsed) {
 					ParseEverything = true;
 					ParseImage(forceReadFromData);
@@ -469,6 +468,9 @@ namespace MapleLib.WzLib {
 
 				var startPos = writer.BaseStream.Position;
 				imgProp.AddPropertiesForWzImageDumping(WzProperties);
+				if (!isWzIvSimilar) {
+					ListWzContainerImpl.ConvertKey(imgProp, wzKey, writer.WzKey);
+				}
 				imgProp.WriteValue(writer);
 
 				writer.StringCache.Clear();

commit 7cc1bcdf006b98c8c26e7f953c077e9e0980ca8f

[MapleLib] Fix subdir saving #44
new iv was not being passed down to subdirectories. Now we pass the same IV and UserKey down the entire GenerateDataFile chain

Regression from https://github.com/Arnuh/HaSuite/commit/e0a9a2351935260f99a8c1dd4f55280a97948ed4

useIv use to be passed down the chain like we are doing again here.

MapleLib/WzLib/WzDirectory.cs
@@ -329,7 +329,11 @@ namespace MapleLib.WzLib {
 		/// <param name="isWzUserKeyDefault">Uses the default MapleStory UserKey or a custom key.</param>
 		/// <param name="prevOpenedStream">The previously opened file stream</param>
 		/// <returns></returns>
-		internal int GenerateDataFile(bool isWzIvSimilar, bool isWzUserKeyDefault, FileStream prevOpenedStream) {
+		internal int GenerateDataFile(bool isWzIvSimilar, byte[] WzIv, bool isWzUserKeyDefault, byte[] UserKey, FileStream prevOpenedStream) {
+			// Parameter 'WzIv' hides field 'byte[] MapleLib.WzLib.WzDirectory.WzIv'
+			// Parameter 'UserKey' hides field 'byte[] MapleLib.WzLib.WzDirectory.UserKey'
+			// These are only used for saving. We don't care about the overriden values because those are only for loading
+			// Which we do elsewhere.
 			size = 0;
 			var entryCount = subDirs.Count + images.Count;
 			if (entryCount == 0) {
@@ -387,7 +391,7 @@ namespace MapleLib.WzLib {
 			foreach (var dir in subDirs) {
 				var nameLen = WzTool.GetWzObjectValueLength(dir.name, 3);
 				size += nameLen;
-				size += dir.GenerateDataFile(isWzIvSimilar, isWzUserKeyDefault, prevOpenedStream);
+				size += dir.GenerateDataFile(isWzIvSimilar, WzIv, isWzUserKeyDefault, UserKey, prevOpenedStream);
 				size += WzTool.GetCompressedIntLength(dir.size);
 				size += WzTool.GetCompressedIntLength(dir.Checksum);
 				size += 4;

MapleLib/WzLib/WzFile.cs
@@ -589,7 +589,7 @@ namespace MapleLib.WzLib {
 				var tempFile = Path.GetFileNameWithoutExtension(path) + ".TEMP";
 
 				using (var fs = new FileStream(tempFile, FileMode.Append, FileAccess.Write)) {
-					wzDir.GenerateDataFile(isWzIvSimilar, isWzUserKeyDefault, fs);
+					wzDir.GenerateDataFile(isWzIvSimilar, WzIv, isWzUserKeyDefault, UserKey, fs);
 				}
 
 				WzTool.StringCache.Clear();